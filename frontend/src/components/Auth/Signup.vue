ecf75a80236b845c4b07e4652d4ba92131f70324
sthylo <63072156+sthylo@users.noreply.github.com>
2020-08-28 15:02:17 +0200

    Ajout regex prénom et nom

--- frontend/src/components/Auth/Signup.vue
+++ frontend/src/components/Auth/Signup.vue
@@ -43,9 +43,11 @@ export default {
             valid: true,
             firstNameRules: [
                 v => !!v || 'Renseignez votre prénom',
+                v => /^[A-Za-z]+$/.test(v) || "Votre prénom n'est pas valide",
             ],
             lastNameRules: [
                 v => !!v || 'Renseignez votre nom',
+                v => /^[A-Za-z]+$/.test(v) || "Votre nom n'est pas valide",
             ],
             emailRules: [
                 v => !!v || 'Renseignez votre e-mail',
